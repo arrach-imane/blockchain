74198f269c0fae7fd26549711b76d1b2cb46fb34
arrach-imane <arrachimane@yahoo.com>
2026-02-13 10:37:48 +0100

    Implement Block class for blockchain structure

---
+++ block.py
@@ -0,0 +1,16 @@
+import hashlib
+import time
+
+class Block:
+    def __init__(self, index, data, previous_hash):
+        self.index = index
+        self.timestamp = time.time()
+        self.data = data
+        self.previous_hash = previous_hash
+        self.nonce = 0          #NOUVEAU
+        self.hash = self.compute_hash()
+
+    def compute_hash(self):
+        block_string = f"{self.index}{self.timestamp}{self.data}{self.previous_hash}{self.nonce}"
+        return hashlib.sha256(block_string.encode()).hexdigest()
+
